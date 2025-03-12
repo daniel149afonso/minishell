42f4b101135dfaca7265a3eda8f9804f444006ad
Alessio Piscopo <apiscopo@c2r3s3.42lausanne.ch>
2025-03-12 21:34:28 +0100

    Change on splitou adding test on main need libft for splitou

--- minitest.c
+++ minitest.c
@@ -6,26 +6,33 @@
 /*   By: apiscopo <apiscopo@student.42lausanne.c    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/12 14:58:19 by apiscopo          #+#    #+#             */
-/*   Updated: 2025/03/12 15:33:27 by apiscopo         ###   ########.fr       */
+/*   Updated: 2025/03/12 21:28:09 by apiscopo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <stdio.h>
-#include <readline/readline.h>
-#include <readline/history.h>
-#include <stdlib.h>
+#include "minishell.h"
 
 int main(void)
 {
-	char *result;
+	char	*input;
+	char	**result;
+	int		i = 0;
 
 	while (1)
 	{
-		result = readline("minishell :");
-		printf("%s\n", result);
-		if (*result)
-			add_history(result);
-		free (result);
+		input = readline("minishell:");
+		if (*input)
+		{
+			result = ft_splitou(input);
+			add_history(input);
+		}
+		while (result[i] != NULL)
+		{
+			printf("%s\n", result[i]);
+			i++;
+		}
+		i = 0;
+		free (input);
 	}
 	return 0;
 }
