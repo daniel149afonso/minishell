2dba95eb381252bdd11e4af7da7b5bf964746723
daniel <daniel-congo2@hotmail.com>
2025-03-13 21:29:32 +0100

    converion du tableau de tokens en liste chainée + ajout libft

--- ft_free_error.c
+++ ft_free_error.c
@@ -6,7 +6,7 @@
 /*   By: daniel149afonso <daniel149afonso@studen    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/12 17:09:06 by daafonso          #+#    #+#             */
-/*   Updated: 2025/03/13 21:17:40 by daniel149af      ###   ########.fr       */
+/*   Updated: 2025/03/13 21:27:03 by daniel149af      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,6 +23,7 @@ void	ft_free_lst(t_list **lst)
 {
 	t_list	*tmp;
 
+	tmp = NULL;
 	while (*lst)
 	{
 		tmp = (*lst)->next;
