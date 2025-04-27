b53832dcaf73315eb175759da1dd2a3cd950d68c
daniel <daniel-congo2@hotmail.com>
2025-04-27 20:18:54 +0200

    echo marche gestion variable en cours

--- commands/echo.c
+++ commands/echo.c
@@ -6,37 +6,51 @@
 /*   By: daniel149afonso <daniel149afonso@studen    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/20 02:25:36 by daniel149af       #+#    #+#             */
-/*   Updated: 2025/04/27 14:08:55 by daniel149af      ###   ########.fr       */
+/*   Updated: 2025/04/27 20:17:29 by daniel149af      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../header/minishell.h"
 
-void	display_with_args(t_list *arg)
+void	display_content(t_list *arg)
 {
-	t_list	*tmp;
-	int		option;
-	int		newline;
-
-	option = 0;
-	newline = 1;
-	if (ft_strcmp((char *)arg->content, "-n") == 0)
-	{
-		option = 1;
-		newline = 0;
-	}
 	while (arg && arg->content)
 	{
-		tmp = arg->next;
-		if (ft_strcmp((char *)arg->content, "-n") != 0)
-			printf("%s", (char *)arg->content);
-		option = 0;
-		arg = tmp;
+		printf("%s", (char *)arg->content);
+		if (arg->next && arg->next->content)
+			printf(" ");
+		arg = arg->next;
 	}
-	(void)option;
-	if (newline == 1)
-		printf("\n");
 }
+
+void	display_inside_quotes(char *str)
+{
+	int		i;
+	char	quote;
+
+	i = 0;
+	quote = '\0';
+	while (str[i])
+	{
+		if (str[i] == '"' || str[i] == '\'')
+		{
+			quote = str[i++];
+			while (str[i] && str[i] != quote)
+			{
+				printf("%c", str[i]);
+				i++;
+			}
+			if (str[i])
+				i++; // passer la fermeture de quote
+		}
+		else
+		{
+			printf("%c", str[i]);
+			i++;
+		}
+	}
+}
+
 void	display_with_args_v2(t_list *arg)
 {
 	int		newline;
@@ -47,13 +61,12 @@ void	display_with_args_v2(t_list *arg)
 		arg = arg->next;
 		newline = 0;
 	}
-	
 	while (arg && arg->content)
 	{
-		printf("%s", (char *)arg->content);
-		if (arg->next && arg->next->content)
-			printf(" ");
+		display_inside_quotes((char *)arg->content);
 		arg = arg->next;
+		if (arg && arg->content)
+			printf(" ");
 	}
 	if (newline)
 		printf("\n");
