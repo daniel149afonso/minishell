f9c535c5e5ac30ab1b93456f9a422d5b7377f3a6
Daniel Afonso <daniel149afonso@users.noreply.github.com>
2025-05-02 16:04:31 +0200

    variable handling in course..

--- commands/echo.c
+++ commands/echo.c
@@ -3,73 +3,23 @@
 /*                                                        :::      ::::::::   */
 /*   echo.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: daniel149afonso <daniel149afonso@studen    +#+  +:+       +#+        */
+/*   By: daafonso <daafonso@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/20 02:25:36 by daniel149af       #+#    #+#             */
-/*   Updated: 2025/05/02 13:21:46 by daniel149af      ###   ########.fr       */
+/*   Updated: 2025/05/02 15:54:32 by daafonso         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../header/minishell.h"
-
-void	handle_single_double(char *str, int first_quote)
-{
-	int	i;
-
-	i = 0;
-	if (first_quote == '\'')
-	{
-		while (str[i])
-		{
-			if (str[i] != '\'')
-				write(1, &str[i], 1);
-			i++;
-		}
-	}
-	else
-	{
-		while (str[i])
-		{
-			if (str[i] != '"')
-				write(1, &str[i], 1);
-			i++;
-		}
-	}
-}
-
-void	find_first_quote(char *str)
-{
-	int	i;
-	int	first_quote;
-
-	i = 0;
-	while (str[i])
-	{
-		if (str[i] == '\'' || str[i] == '"')
-		{
-			first_quote = str[i];
-			handle_single_double(str, first_quote);
-			break ;
-		}
-		
-	}
-	
-}
 
 void	display_inside_quotes(char *str)
 {
 	int		i;
 
 	i = 0;
-	if (ft_strchr(str, '\'') && ft_strchr(str, '"'))
-	{
-		find_first_quote(str);
-		return ;
-	}
 	while (str[i])
 	{
-		if ((str[i] != '\'' && str[i] != '"'))
-			write(1, &str[i], 1);
+		write(1, &str[i], 1);
 		i++;
 	}
 }
@@ -86,7 +36,8 @@ void	display_with_args(t_list *arg)
 	}
 	while (arg && arg->content)
 	{
-		display_inside_quotes((char *)arg->content);
+		//display_inside_quotes((char *)arg->content);
+		printf("%s", (char *)arg->content);
 		arg = arg->next;
 		if (arg && arg->content)
 			printf(" ");
