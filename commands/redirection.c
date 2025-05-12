8942e15ddf02b3ce54ec732890701d8cef9aa3b2
daniel <daniel-congo2@hotmail.com>
2025-05-12 15:44:18 +0200

    gestion redirection en cours

--- commands/redirection.c
+++ commands/redirection.c
@@ -6,44 +6,46 @@
 /*   By: daniel149afonso <daniel149afonso@studen    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/06 17:04:38 by daniel149af       #+#    #+#             */
-/*   Updated: 2025/05/11 16:05:18 by daniel149af      ###   ########.fr       */
+/*   Updated: 2025/05/12 01:11:57 by daniel149af      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../header/minishell.h"
 
-int	is_redirection(t_g *g, t_list *lst)
+int	one_redirection(t_g *g, t_list *redir)
 {
-	t_list	*file;
 	int		fd;
-	int		res;
-	
-	res = 0;
-	file = NULL;
-	while (lst)
+
+	fd = open((char *)redir->next->content,
+		O_WRONLY | O_CREAT | O_TRUNC, 0644);
+	if (fd < 0)
 	{
-		if (ft_strcmp((char *)lst->content, ">") == 0)
-		{
-			res = 1;
-			file = lst->next;
-			fd = open((char *)file->content, O_WRONLY | O_CREAT | O_TRUNC, 0644);
-			if (fd < 0)
-			{
-				perror("open failed");
-				return (0);
-			}
-			// 💾 sauvegarder stdout
-			g->s_stdout = dup(STDOUT_FILENO);
-
-			// 🔁 rediriger stdout vers le fichier
-			dup2(fd, STDOUT_FILENO);
-			close(fd);
-
-			// 🔁 restaurer stdout
-			// dup2(original_stdout, STDOUT_FILENO);
-			// close(original_stdout);
-		}
-		lst = lst->next;
+		perror("open failed");
+		return (1);
 	}
-	return (res);
+	// 💾 sauvegarder stdout
+	g->s_stdout = dup(STDOUT_FILENO);
+
+	// 🔁 rediriger stdout vers le fichier
+	dup2(fd, STDOUT_FILENO);
+	close(fd);
+
+	// 🔁 restaurer stdout
+	// dup2(original_stdout, STDOUT_FILENO);
+	// close(original_stdout);
+	return (0);
+}
+
+int	is_redirection(t_g *g)
+{
+	t_list	*tmp;
+
+	tmp = g->lst;
+	while (tmp)
+	{
+		if (ft_strcmp((char *)tmp->content, ">") == 0)
+			one_redirection(g, tmp);
+		tmp = tmp->next;
+	}
+	return (0);
 }
