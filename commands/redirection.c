bc2e2b320e3cf8af5d173ac51dc552661084df6b
daniel <daniel-congo2@hotmail.com>
2025-05-14 18:13:39 +0200

    double redir en cours

--- commands/redirection.c
+++ commands/redirection.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   redirection.c                                      :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: daafonso <daafonso@student.42.fr>          +#+  +:+       +#+        */
+/*   By: daniel149afonso <daniel149afonso@studen    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/06 17:04:38 by daniel149af       #+#    #+#             */
-/*   Updated: 2025/05/13 18:20:19 by daafonso         ###   ########.fr       */
+/*   Updated: 2025/05/14 18:11:37 by daniel149af      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -68,6 +68,30 @@ int	one_redirection(t_g *g, t_list *redir)
 	return (0);
 }
 
+int	double_redirection(t_g *g, t_list *redir)
+{
+	int		fd;
+
+	fd = open((char *)redir->next->content,
+		O_WRONLY | O_CREAT, 0644);
+	if (fd < 0)
+	{
+		perror("open failed");
+		return (1);
+	}
+	// 💾 sauvegarder stdout
+	g->s_stdout = dup(STDOUT_FILENO);
+
+	// 🔁 rediriger stdout vers le fichier
+	dup2(fd, STDOUT_FILENO);
+	close(fd);
+
+	// 🔁 restaurer stdout
+	// dup2(original_stdout, STDOUT_FILENO);
+	// close(original_stdout);
+	return (0);
+}
+
 int	is_redirection(t_g *g)
 {
 	t_list	*tmp;
@@ -77,6 +101,8 @@ int	is_redirection(t_g *g)
 	{
 		if (ft_strcmp((char *)tmp->content, ">") == 0)
 			one_redirection(g, tmp);
+		if (ft_strcmp((char *)tmp->content, ">>") == 0)
+			double_redirection(g, tmp);
 		tmp = tmp->next;
 	}
 	return (0);
