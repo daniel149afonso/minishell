b32c6765ea6df838aea7f1ba71d208b521b82bad
daniel <daniel-congo2@hotmail.com>
2025-05-28 01:17:30 +0200

    redirection fini

--- parsing/handle_var.c
+++ parsing/handle_var.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   handle_var.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: daafonso <daafonso@student.42.fr>          +#+  +:+       +#+        */
+/*   By: daniel149afonso <daniel149afonso@studen    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/30 20:15:35 by daafonso          #+#    #+#             */
-/*   Updated: 2025/05/26 20:55:33 by daafonso         ###   ########.fr       */
+/*   Updated: 2025/05/28 00:34:28 by daniel149af      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -36,35 +36,44 @@ void	handle_variable_expansion(char *str, char **res, int *i, t_env *env)
 	free(var);
 }
 
+void	add_char_to_result(char *str, char **res, int *i)
+{
+	char	c[2];
+
+	c[0] = str[*i];
+	c[1] = '\0';
+	*res = ft_join_and_free(*res, c);
+	(*i)++;
+}
+
 char	*expand_variables(char *str, t_env *env)
 {
 	int		i;
 	int		in_single;
+	int		in_double;
 	char	*res;
-	char	c[2];
 
 	i = 0;
 	in_single = 0;
+	in_double = 0;
 	res = ft_calloc(1, 1);
 	if (!res)
 		return (NULL);
 	while (str[i])
 	{
-		if (str[i] == '\'')
-			handle_single_quote(str, &res, &i, &in_single);
+		if (str[i] == '\'' && !in_double)
+			in_single = !in_single;
+		else if (str[i] == '"' && !in_single)
+			in_double = !in_double;
 		else if (str[i] == '$' && !in_single)
-			handle_variable_expansion(str, &res, &i, env);
-		else
 		{
-			c[0] = str[i];
-			c[1] = '\0';
-			res = ft_join_and_free(res, c);
-			i++;
+			handle_variable_expansion(str, &res, &i, env);
+			continue ;
 		}
+		add_char_to_result(str, &res, &i);
 	}
 	return (res);
 }
-
 
 char	**search_var(char **strs, t_env *env)
 {
